0a5748eefa544ea42d0c017ff225566534995aee
Hari <anarahari@gmail.com>
2017-09-26 00:23:07 +0800

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='pyflightdata',
-    version='0.5.1',
+    version='0.5.2',
     description='Get flight data from websites by making HTTP calls',
     long_description='Please visit https://github.com/supercoderz/pyflightdata for more details',
     author='Narahari Allamraju',
